6a36b1585522b709a7a1a0ed9c65554ee7e4abff
wangkaiwd <1162486176@qq.com>
2018-11-02 21:46:59 +0800

    删除多余文件

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react'
+
+class App extends Component {
+  state = {
+    message: [],
+    inputMsg: ''
+  }
+
+  render () {
+    return (
+      <div>
+        App
+      </div>
+    )
+  }
+}
+
+export default App
--- src/index.js
+++ src/index.js
@@ -1,10 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { HashRouter as Router } from 'react-router-dom'
-import Admin from './admin'
+import App from './App'
 import registerServiceWorker from './registerServiceWorker'
 import 'styles/reset.less'
 import 'styles/container.less'
 
-ReactDOM.render(<Router><Admin/></Router>, document.getElementById('root'))
+ReactDOM.render(<App/>, document.getElementById('root'))
 registerServiceWorker()
