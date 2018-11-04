57f20d9077a4186bdf7da852170d96a15fc971b8
wangkaiwd <1162486176@qq.com>
2018-11-04 19:38:30 +0800

    feat: 动态添加表单配置项来渲染表单的生成

--- src/pages/FormHandle/AntdForm.js
+++ src/pages/FormHandle/AntdForm.js
@@ -1,106 +1,122 @@
 import React, { Component } from 'react'
-import { Form, Input, Icon, Button } from 'antd'
+import { Form, Input, Icon, Button, DatePicker, Select } from 'antd'
 
 const FormItem = Form.Item
+const Option = Select.Option
+const formItemLayout = {
+  labelCol: {
+    xs: {span: 24},
+    sm: {span: 6},
+  },
+  wrapperCol: {
+    xs: {span: 24},
+    sm: {span: 12},
+  },
+}
+const formMeta = {
+  colon: true,
+  elements: [
+    {
+      key: 'username',
+      formItemProps: {
+        label: '用户名',
+      },
+      widget: Input,
+      widgetProps: {
+        style: {width: '80%'},
+        placeholder: '请输入用户名'
+      },
+      rules: []
+    },
+    {
+      key: 'password',
+      formItemProps: {
+        label: '密码',
+      },
+      widget: Input,
+      widgetProps: {
+        style: {width: '80%'},
+        placeholder: '请输入密码'
+      },
+      rules: [
+        {required: true, message: 'Please input your username!'}
+      ]
+    }
+  ]
+
+}
 
 class DynamicFieldSet extends Component {
-  remove = (k) => {
-    const {form} = this.props
-    // can use data-binding to get
-    const keys = form.getFieldValue('keys')
-    // We need at least one passenger
-    if (keys.length === 1) {
-      return
+  state = {
+    count: 0
+  }
+  addClick = () => {
+    let {count} = this.state
+    count++
+    const formOptions = {
+      key: `username${count}`,
+      formItemProps: {
+        label: `用户名${count}`,
+      },
+      widget: Select,
+      widgetProps: {
+        style: {width: '60%'},
+        placeholder: `请输入用户名${count}`
+      },
+      selectOptions: (
+        [
+          <Option key={1} value={1}>1</Option>,
+          <Option key={2} value={2}>2</Option>,
+          <Option key={3} value={3}>3</Option>,
+        ]
+      )
     }
-
-    // can use data-binding to set
-    form.setFieldsValue({
-      keys: keys.filter(key => key !== k),
-    })
-  }
-
-  add = () => {
-    const {form} = this.props
-    // can use data-binding to get
-    const keys = form.getFieldValue('keys')
-    const nextKeys = keys.concat(keys.length)
-    // can use data-binding to set
-    // important! notify form to detect changes
-    form.setFieldsValue({
-      keys: nextKeys,
-    })
-  }
-
-  handleSubmit = (e) => {
-    e.preventDefault()
-    this.props.form.validateFields((err, values) => {
-      if (!err) {
-        console.log('Received values of form: ', values)
+    const formOptions2 = {
+      key: `username1${count}`,
+      formItemProps: {
+        label: `时间${count}`,
+      },
+      widget: DatePicker,
+      widgetProps: {
+        style: {width: '60%'},
+        placeholder: `请输入用户名${count}`
       }
-    })
+    }
+    formMeta.elements.push(formOptions, formOptions2)
+    // formMeta.elements.push(formOptions2)
+    this.setState({count})
+  }
+  remove = (key) => {
+    const {elements} = formMeta
+    const index = elements.findIndex(element => element.key === key)
+    console.log(index)
+    elements.splice(index, 1)
+    this.setState({})
   }
 
   render () {
     const {getFieldDecorator, getFieldValue} = this.props.form
-    const formItemLayout = {
-      labelCol: {
-        xs: {span: 24},
-        sm: {span: 4},
-      },
-      wrapperCol: {
-        xs: {span: 24},
-        sm: {span: 20},
-      },
-    }
-    const formItemLayoutWithOutLabel = {
-      wrapperCol: {
-        xs: {span: 24, offset: 0},
-        sm: {span: 20, offset: 4},
-      },
-    }
-    getFieldDecorator('keys', {initialValue: []})
-    const keys = getFieldValue('keys')
-    const formItems = keys.map((k, index) => {
-      return (
-        <FormItem
-          {...(index === 0 ? formItemLayout : formItemLayoutWithOutLabel)}
-          label={index === 0 ? 'Passengers' : ''}
-          required={false}
-          key={k}
-        >
-          {getFieldDecorator(`names[${k}]`, {
-            validateTrigger: ['onChange', 'onBlur'],
-            rules: [{
-              required: true,
-              whitespace: true,
-              message: 'Please input passenger\'s name or delete this field.',
-            }],
-          })(
-            <Input placeholder="passenger name" style={{width: '60%', marginRight: 8}}/>
-          )}
-          {keys.length > 1 ? (
-            <Icon
-              className="dynamic-delete-button"
-              type="minus-circle-o"
-              disabled={keys.length === 1}
-              onClick={() => this.remove(k)}
-            />
-          ) : null}
-        </FormItem>
-      )
-    })
     return (
-      <Form onSubmit={this.handleSubmit}>
-        {formItems}
-        <FormItem {...formItemLayoutWithOutLabel}>
-          <Button type="dashed" onClick={this.add} style={{width: '60%'}}>
-            <Icon type="plus"/> Add field
-          </Button>
-        </FormItem>
-        <FormItem {...formItemLayoutWithOutLabel}>
-          <Button type="primary" htmlType="submit">Submit</Button>
-        </FormItem>
-      </Form>
+      <div>
+        <Form style={{maxHeight: '80vh', overflowY: 'scroll'}}>
+          {formMeta.elements.map(element => (
+            <FormItem {...formItemLayout} key={element.key} {...(element.formItemProps)}>
+              {getFieldDecorator(element.key, {
+                // rules: [{required: true, message: 'Please input your username!'}],
+                rules: element.rules
+              })(
+                <element.widget {...(element.widgetProps)}>
+                  {element.selectOptions}
+                </element.widget>
+              )}
+              <Icon style={{fontSize: '24px', position: 'relative', top: '4px', left: '6px'}}
+                    onClick={() => this.remove(element.key)} type="minus-circle"
+                    theme="outlined"/>
+            </FormItem>
+          ))}
+        </Form>
+        <Button type={'primary'} onClick={this.addClick}>添加表单</Button>
+      </div>
     )
   }
 }
