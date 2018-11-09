2859a13b2c2c1bb5c7e4bcddb2b3554ffbcc3213
wangkaiwd <1162486176@qq.com>
2018-11-09 20:13:36 +0800

    feat: 添加懒加载再次失败

--- src/router.js
+++ src/router.js
@@ -21,3 +21,54 @@ export default () => (
     </App>
   </Router>
 )
+// import React, { lazy, Suspense } from 'react';
+// import { HashRouter as Router, Route, Switch } from "react-router-dom";
+
+// const RouterLis = [
+  // {
+  //   component: lazy(() => import("./pages/Chat")),
+  //   path: "/chat"
+  // },
+  // {
+  //   component: lazy(() => import("../containers/Home")),
+  //   path: "/home"
+  // },
+  // {
+  //   component: lazy(() => import("../containers/My")),
+  //   path: "/my"
+  // },
+  // {
+  //   component: lazy(() => import("../containers/UserInfo")),
+  //   path: "/my/userInfo"
+  // },
+  // {
+  //   component: lazy(() => import("../containers/Works")),
+  //   path: "/works"
+  // },
+  // {
+  //   component: lazy(() => import("../containers/CourseDetail")),
+  //   path: "/courseDetail/:id"
+  // },
+  // {
+  //   component: lazy(() => import("../containers/OtherCourse")),
+  //   path: "/otherCourse/:id"
+  // },
+  // {
+  //   component: lazy(() => import("../containers/WorkDetail")),
+  //   path: "/workDetail/:id"
+  // }
+// ];
+
+// const RouterList = () => (
+//   <Router>
+//     <Suspense>
+//       <Switch>
+//         {RouterLis.map((item, key) => {
+//           return <Route key={key} path={item.path} component={item.component} />
+//         })}
+//       </Switch>
+//     </Suspense>
+//   </Router>
+// )
+
+// export default RouterList;
