bc41b25721d276d348a9f6a9634aca7802479302
wangkaiwd <1162486176@qq.com>
2018-11-14 22:24:30 +0800

    feat: 需求目标定义

---
+++ src/pages/demo/demo.js
@@ -0,0 +1,23 @@
+const data = [
+  { id: 'r1', color: '颜色1', capbitility: '容量1', other: '其它' },
+  { id: 'r2', color: '颜色1', capbitility: '容量2', other: '其它' },
+  { id: 'r3', color: '颜色2', capbitility: '容量1', other: '其它' }
+]
+
+const columns = [
+  {
+    title: 'T+编码',
+    dataIndex: 'priceTsn',
+    width: 200,
+  },
+  {
+    title: '颜色',
+    dataIndex: 'color',
+    width: 150
+  },
+  {
+    title: '容量',
+    dataIndex: 'capbitility',
+    width: 150
+  }
+]
