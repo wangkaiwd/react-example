0c1ccff249349eee5405b60558ec784b09c9dce0
wangkaiwd <1162486176@qq.com>
2018-12-09 21:24:44 +0800

    feat: 通过connect进行组件和store的连接

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import RouterConfig from './router'
+import store from '@/store'
 // PWA  progressive web application
 // 奖项目部署到支持https协议的服务器上，会在断网的时候提供离线访问
 import registerServiceWorker from './registerServiceWorker'
 import 'styles/reset.less'
 import 'styles/container.less'
+import { Provider } from 'react-redux'
 // ReactDOM.render(element,container,[callback])
 // 渲染一个React元素，添加到位于提供的container里的DOM元素中，并返回这个组件的一个引用
 // 如果这个React元素之前已经被渲染到container里去了，这段代码就会进行一次更新，并且只会改变那些反应元素最新状态所必须的DOM元素
 // 回调函数时可选的。如果你提供了，程序会在渲染或更新之后执行这个函数
-ReactDOM.render(<RouterConfig />, document.getElementById('root'))
+ReactDOM.render(
+  <Provider store={store}>
+    <RouterConfig />
+  </Provider>
+  , document.getElementById('root'))
 registerServiceWorker()
