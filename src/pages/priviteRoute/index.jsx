f46b1b249c735d3e7a49abb1b5932ac3162e90cf
wangkaiwd <1162486176@qq.com>
2018-12-22 20:34:57 +0800

    feat: 登录逻辑梳理

--- src/pages/priviteRoute/index.jsx
+++ src/pages/priviteRoute/index.jsx
@@ -13,6 +13,16 @@ import {
 // 3. Log in
 // 4. Click the back button, note the URL each time
 
+// 需求整理：
+//   1. public路由就可以随意访问
+//   2. protected路由需要登录后才能访问
+//        1. 点击protected会对内容权限进行一个判断：有权限的话展示对应的组件，没有权限展示登录组件
+//        2. 在加载到登录组件的时候，会通过state将来之前的路由地址传来，点击登陆后跳转到之前的路由
+
+// 实现功能：登录之后可以跳转回之前的页面，而之前的页面是通过路由参数传递来的，没有传的话默认为根路径'/'
+//          或者通过本地存储实现：跳转之前记录来之前的路径，存到本地存储中，到了登录页面之后，登陆之后将地址从
+//          本地存储中取出，进行跳转
+
 function AuthExample() {
   return (
     <Router>
@@ -84,6 +94,7 @@ const AuthButton = withRouter(
       )
 );
 // 通过props属性传递来
+// render:
 function PrivateRoute({ component: Component, ...rest }) {
   return (
     <Route
@@ -108,6 +119,7 @@ function Public() {
   return <h3>Public</h3>;
 }
 
+// Protected组件只有在login后才能看到
 function Protected() {
   return <h3>Protected</h3>;
 }
@@ -116,12 +128,14 @@ class Login extends React.Component {
   state = { redirectToReferrer: false };
 
   login = () => {
+    // 会让校验规则变为true，并且通过cb回调函数来将重定向定义为true
     fakeAuth.authenticate(() => {
       this.setState({ redirectToReferrer: true });
     });
   };
 
   render() {
+    // 获取通过react-router中通过state传来的参数
     let { from } = this.props.location.state || { from: { pathname: "/" } };
     let { redirectToReferrer } = this.state;
 
