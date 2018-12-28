4ef6434848ef3126900cf5624f6e9f3f95dd22c3
wangkaiwd <1162486176@qq.com>
2018-12-28 20:26:59 +0800

    feat: 获取高阶组件包装后的子组件实例

--- src/pages/Testing/Example.jsx
+++ src/pages/Testing/Example.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { withRouter } from 'react-router-dom'
 import { Button, Row, Col } from 'antd';
 const enString = {
   submit: 'Submit',
@@ -23,6 +24,10 @@ class LocaleSample extends Component {
     locale = (locale === enString ? cnString : enString)
     this.setState({ locale })
   }
+  componentDidMount() {
+    console.log('localeButton', this.localeButton)
+    console.log('localeButton1', this.localeButton1)
+  }
   render() {
     const { locale } = this.state
     // react组件允许Consumers订阅context的改变
@@ -35,29 +40,47 @@ class LocaleSample extends Component {
           <Col>
             <Button type="primary" onClick={this.toggleLocale}>切换语言</Button>
           </Col>
-          <LocaleButton />
+          <LocaleButton
+            getInstance={(localeButton1) => this.localeButton1 = localeButton1}
+          // ref={(button) => this.localeButton = button}
+          // 这样不仅无法获取到子组件实例，而且还会报错：
+          // function components cannot be given refs. Attemps to access this ref will fail
+          // 函数组件不能设置refs,企图使用ref将失败
+          />
         </Row>
       </Provider>
 
     );
   }
 }
-const LocaleButton = () => {
-  // 接受一个函数作为子节点.函数接受当前context的值并返回一个react节点
-  //  <Consumer>
-  //   {context => /* render something based on the context value */}
-  // </Consumer>
-  return (
-    <Consumer>
-      {({ cancel, submit }) => {
-        return (
-          <Col>
-            <Button type="primary">{submit}</Button>
-            <Button type="primary">{cancel}</Button>
-          </Col>
-        )
-      }}
-    </Consumer>
-  )
+
+@withRouter
+class LocaleButton extends Component {
+  refFunc = () => {
+    console.log('refFunc')
+  }
+  componentDidMount = () => {
+    this.props.getInstance(this)
+  }
+  render() {
+    return (
+      <Consumer>
+        {({ cancel, submit }) => {
+          return (
+            <Col>
+              <Button type="primary">{submit}</Button>
+              <Button type="primary">{cancel}</Button>
+            </Col>
+          )
+        }}
+      </Consumer>
+    )
+  }
 }
+
 export default LocaleSample;
+// react中通过ref获取子组件实例存在的问题：
+//      1. 如果组件是普通组件的话，可以直接获取到
+//      2. 如果组件是用HOCs包装过的话，获取的实例是包装组件，并不是子组件
+//            解决方法：父组件通过props传入一个函数到子组件，子组件调用函数，并将子组件的this传给父组件
+//                    之后父组件在子组件执行函数的时候将传来的参数挂载到自己的实例this上，这样就可以获取到子组件的方法和属性并能直接调用
