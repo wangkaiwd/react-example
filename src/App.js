fe7204f3faa6b8bf77dec2f6a64ffd8604e081a2
wangkaiwd <1162486176@qq.com>
2019-05-09 20:24:15 +0800

    feat: 简化理解方式

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { withRouter } from 'react-router-dom'
 
 // 之前好像有理解错误，通过Route的component或者render加载的组件自身就含有this.props.history,this.props.locaton等属性
 // 不是通过上述情况加载的组件想要使用路由相关的属性和方法的时候要通过withRouer包装之后才可以
+// 简单来讲就是非路由组件要使用路由方法需要通过withRouer来进行包装
 @withRouter
 class App extends Component {
   render() {
