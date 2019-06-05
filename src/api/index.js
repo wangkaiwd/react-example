791547c0d4c4ecf9920f4ed8b56efa8d80ea7865
wangkaiwd <1162486176@qq.com>
2019-06-05 11:40:44 +0800

    feat: 项目使用create-react-app 进行重置

--- src/api/index.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by Administrator on 2018/9/15/015
- */
-import { ajax } from '@/http/ajax';
-
-export const fetchDetailList = ajax('/detail');
-export const fetchCityList = ajax('/detail');
-export const fetchHome = ajax('/home');
--- src/http/reqUrls.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by wangkai on 2019/1/23
- * Desc: 请求地址
- */
-
-const baseUrl = 'https://www.easy-mock.com/mock/5b2e15c2d901cc25e7df4e0c/travel';
-export default baseUrl;
