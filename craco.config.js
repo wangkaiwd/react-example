7f5a589ec1de9430a8d0d042de4dc8c73fd02568
wangkaiwd <1162486176@qq.com>
2019-06-05 18:31:53 +0800

    feat: 添加scss normalize文件

--- craco.config.js
+++ craco.config.js
@@ -1,5 +1,8 @@
 const CracoAntDesignPlugin = require('craco-antd');
-
+const { whenDev, whenProd } = require('@craco/craco');
 module.exports = {
+  eslint: {
+    enable: false
+  },
   plugins: [{ plugin: CracoAntDesignPlugin }]
 };
--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react';
 import { Button } from 'antd';
+import './assets/styles/base.scss';
 
 function App () {
   return (
     <div className="App">
       App
       <Button type={'primary'}>antDesign按钮</Button>
+      <a href="javascript:;">link</a>
+      <ul>
+        <li>内容1</li>
+        <li>内容2</li>
+        <li>内容3</li>
+        <li>内容4</li>
+        <li>内容5</li>
+        <li>内容6</li>
+        <li>内容7</li>
+        <li>内容8</li>
+        <li>内容9</li>
+        <li>内容10</li>
+      </ul>
     </div>
   );
 }
