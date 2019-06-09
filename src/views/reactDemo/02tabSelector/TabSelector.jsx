60db70535e3c88465056d3c7c8fd9963c29b08d9
wangkaiwd <1162486176@qq.com>
2019-06-09 17:26:18 +0800

    feat: 将selected设置为必传，在很大程度上避免了出现的bug

--- src/views/reactDemo/02tabSelector/TabSelector.jsx
+++ src/views/reactDemo/02tabSelector/TabSelector.jsx
@@ -6,8 +6,8 @@ import './TabSelector.scss';
 /**
  * 组件设计时的几个考虑：
  *  1. 为什么要将selected通过props传入，而不是通过内部的state来进行控制
- *      如果通过state来控制的话，用户并不能修改初始时选中项，这在用户不关心内部的选中项，只是关心点击后选中内容的时候可以使用。
- *      而如果用户要完全控制选中项的时候：比如初始时选中最后一项，我们就需要通过`props`来将`state`进行传入
+ *      如果通过state来控制的话，用户并不能修改选中项，这在用户不关心内部的选中项，只是关心点击后选中内容的时候可以使用。
+ *      而如果用户要完全控制选中项的时候：比如要设置选中项，我们就需要通过`props`来将`state`进行传入
  *  2. 为什么要还传入一个onChange函数
  *      onChange函数的作用：
  *        1. 暴露一个api：让用户可以在tab切换的时候做一些事情
@@ -44,12 +44,11 @@ class TabSelector extends Component {
 }
 TabSelector.propTypes = {
   options: PropTypes.arrayOf(PropTypes.object),
-  selected: PropTypes.string,
+  selected: PropTypes.string.isRequired,
   onChange: PropTypes.func
 };
 TabSelector.defaultProps = {
   options: [],
-  selected: '',
   onChange: () => {}
 };
 class TabSelectorSample extends Component {
