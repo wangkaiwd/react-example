cf9a642520306d27cda7d9359b93f8e06358b329
wangkaiwd <1162486176@qq.com>
2019-06-10 10:56:50 +0800

    feat: 将Sider的width属性设置为可配置的

--- src/layout/DemoSider/DemoSider.jsx
+++ src/layout/DemoSider/DemoSider.jsx
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import { Layout } from 'antd';
 import SideMenu from 'components/sideMenu';
+import PropTypes from 'prop-types';
 import './DemoSider.scss';
 
 const { Sider } = Layout;
-
 class DemoSider extends Component {
   render () {
+    const { width } = this.props;
     return (
-      <Sider width={260} className={'demo-sider'}>
+      <Sider width={width} className={'demo-sider'}>
         <div className="logo">React-Example</div>
         <SideMenu/>
       </Sider>
     );
   }
 }
-
+DemoSider.propTypes = {
+  width: PropTypes.number
+};
+DemoSider.defaultProps = {
+  width: 260
+};
 export default DemoSider;
